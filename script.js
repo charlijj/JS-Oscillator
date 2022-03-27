522d8600660f33b4412fe13bb3baaafa668d7dba
Jasper <jjcharlinski@gmail.com>
2022-03-27 00:07:37 -0700

    Paused option added to play demo button

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-
-document.addEventListener('keydown', function () {
+// For typing activation
+// ----------------------------------------------------------------------------------------------------------
+document.addEventListener(`keydown`, function () {
 
   noteOn(event.code, event.repeat);
 
 });
 
 
-let i=0;
 function noteOn (key, repeat) {
 
+
+  // Removes overlapping distortion when key is pressed
+  if (repeat)
+  {
+    return;
+  }
+
+  // Remove any previous effects on the gain node
   gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
 
   let active_key;
-
   gainNode.gain.value = 0.33;
 
   console.log(key);
@@ -21,107 +28,110 @@ function noteOn (key, repeat) {
   switch (event.code)
   {
     // C3
-    case 'KeyZ':
+    case `KeyZ`:
       note = notelist.c3;
-      active_key = 'c3';
+      active_key = `c3`;
       break;
 
     // D3
-    case 'KeyX':
+    case `KeyX`:
       note = notelist.d3;
-      active_key = 'd3';
+      active_key = `d3`;
       break;
     // E3
-    case 'KeyC':
+    case `KeyC`:
       note = notelist.e3;
-      active_key = 'e3';
+      active_key = `e3`;
       break;
     // F3
-    case 'KeyV':
+    case `KeyV`:
       note = notelist.f3;
-      active_key = 'f3';
+      active_key = `f3`;
       break;
     // G3
-    case 'KeyB':
+    case `KeyB`:
       note = notelist.g3;
-      active_key = 'g3';
+      active_key = `g3`;
       break;
     // A3
-    case 'KeyN':
+    case `KeyN`:
       note = notelist.a3;
-      active_key = 'a3';
+      active_key = `a3`;
       break;
     // B3
-    case 'KeyM':
+    case `KeyM`:
       note = notelist.b3;
-      active_key = 'b3';
+      active_key = `b3`;
       break;
     // B3
-    case 'KeyA':
+    case `KeyA`:
       note = notelist.b3;
-      active_key = 'b3';
+      active_key = `b3`;
       break;
     // C4
-    case 'KeyS':
+    case `KeyS`:
       note = notelist.c4;
-      active_key = 'c4';
+      active_key = `c4`;
       break;
     // D4
-    case 'KeyD':
+    case `KeyD`:
       note = notelist.d4;
-      active_key = 'd4';
+      active_key = `d4`;
       break;
     // E4
-    case 'KeyF':
+    case `KeyF`:
       note = notelist.e4;
-      active_key = 'e4';
+      active_key = `e4`;
       break;
     // F4
-    case 'KeyG':
+    case `KeyG`:
       note = notelist.f4;
-      active_key = 'f4';
+      active_key = `f4`;
       break;
     // G4
-    case 'KeyH':
+    case `KeyH`:
       note = notelist.g4;
-      active_key = 'g4';
+      active_key = `g4`;
       break;
     // A4
-    case 'KeyJ':
+    case `KeyJ`:
       note = notelist.a4;
-      active_key = 'a4';
+      active_key = `a4`;
       break;
     // B4
-    case 'KeyK':
+    case `KeyK`:
       note = notelist.b4;
-      active_key = 'b4';
+      active_key = `b4`;
       break;
 
     // C5
-    case 'KeyL':
+    case `KeyL`:
       note = notelist.c5;
-      active_key = 'c5';
+      active_key = `c5`;
       break;
     default:
-      alert('Key has not been mapped yet');
+      alert(`Key has not been mapped yet`);
 
   }
 
+  // To determine sustain depending on waveform
   switch (wave)
   {
 
-    case 'sine':
+    case `sine`:
       sustain = 0.3;
       break;
-    case 'sine':
+    case `sine`:
       sustain = 0.3;
       break;
     default:
       sustain = 0.5;
   }
 
-  document.getElementById(active_key).style.backgroundColor = 'grey';
-  setTimeout(function () {document.getElementById(active_key).style.backgroundColor = 'white';}, 500);
+  let timeoutsus = sustain * 1000;
+  console.log(timeoutsus);
+  document.getElementById(active_key).style.backgroundColor = `grey`;
+  setTimeout(function () {document.getElementById(active_key).style.backgroundColor = `white`;}, timeoutsus);
 
   const osci = audioCtx.createOscillator();
   osci.type = wave;
@@ -131,55 +141,30 @@ function noteOn (key, repeat) {
   
   gainNode.gain.setValueAtTime(0.33, audioCtx.currentTime);
 
-  
-  console.log('repeat: ' + repeat);
-
   gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + sustain);
   osci.start();
-  setTimeout(function () {osci.stop()}, 550);
-
-
-
+  setTimeout(function () {osci.stop()}, timeoutsus);
 
 }
 
 
-// ----------------------------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------------------------------------------------
 
 // Div of canvas
-const container = document.getElementById('container');
+const container = document.getElementById(`container`);
 
 // Drawing area
-const canvas = document.getElementById('canvas1');
+const canvas = document.getElementById(`canvas1`);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // To get acsess to 2D library
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext(`2d`);
 
 // ----------------------------------------------------------------------------------
-
-let wave;
-wave = 'square';
-
-let sustain;
-
-const waveForm = document.getElementById('waveForm');
-waveForm.value = wave;
-waveForm.onchange = function () {wave = waveForm.value};
-console.log("now playing: " + wave);
-
-const playDemo = document.getElementById('playDemo');
-playDemo.addEventListener('click', function () {
-  console.log('playing demo'); 
-  demo.play();
-  if (!demo.paused)
-  {
-    
-  };
-
-
-});
 
 const notelist = {
 
@@ -201,23 +186,61 @@ const notelist = {
 
   c5:523.25
 };
-
-
 // Main audio compenent
 const audioCtx = new AudioContext();
-const demo = new Audio('demo.mp3');
+
+// Variable to hold waveform type value
+let wave;
+wave = `square`;
+// Variable to holde sustin value
+let sustain;
+
+const demo = new Audio(`demo.mp3`);
 let audioSource = audioCtx.createMediaElementSource(demo);
 
 // Gain for oscillator
 let gainNode = audioCtx.createGain();
 
+// Gain for Demo
+let demoGain = audioCtx.createGain();
+demoGain.gain.setValueAtTime(0.20, audioCtx.currentTime);
+
+// Waveform dropdown
+const waveForm = document.getElementById(`waveForm`);
+waveForm.value = wave;
+waveForm.onchange = function () {wave = waveForm.value};
+console.log(`now playing: ` + wave);
+
+const playPauseDemo = document.getElementById(`playDemo`);
+playPauseDemo.value = `Play Demo`;
+let playPauseText = playPauseDemo.value;
+
+playPauseDemo.addEventListener(`click`, function () {
+
+    if (demo.paused)
+    {
+      demo.play();
+      playPauseDemo.value = `Pause`;
+      console.log(`playing demo`); 
+      console.log(playPauseDemo.value);
+    }
+    else
+    {
+      demo.pause();
+      playPauseDemo.value = `Play Demo`;
+      console.log(`pausing demo`);
+    }
+
+});
+
 // Analyzer for drawing graph
 let analyzer = audioCtx.createAnalyser();
+audioSource.connect(demoGain);
 gainNode.connect(analyzer);
-audioSource.connect(analyzer);
+demoGain.connect(analyzer);
 analyzer.connect(audioCtx.destination);
 
-// Half of fftSize, is the number  of bars to be drawn by vizulizer
+// Half of fftSize, is the number of bars to be drawn by vizulizer
 analyzer.fftSize = 1024;
 
 const bufferLen = analyzer.frequencyBinCount;
@@ -242,7 +265,7 @@ function animate () {
   for (let i = 0; i < bufferLen; i++)
   {
       barHeight = dataArray[i] + 250;
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = `red`;
       ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
       x += barWidth + 5;
   }
