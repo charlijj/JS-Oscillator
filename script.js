107999722db5ea8c48d1eeed0cdbfdbc3a82fbf0
Jasper <jjcharlinski@gmail.com>
2022-04-26 19:17:42 -0700

    black keys added

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener(`keydown`, function () {
 function noteOn (key, repeat) {
 
 
-  // Removes overlapping distortion when key is pressed
+  // Removes overlapping distortion when key is held
   if (repeat)
   {
     return;
@@ -32,11 +32,20 @@ function noteOn (key, repeat) {
       note = notelist.c3;
       active_key = `c3`;
       break;
-
+    // C#3
+    case `KeyW`:
+      note = notelist.c3sharp;
+      active_key = `c3sharp`;
+      break;
     // D3
     case `KeyX`:
       note = notelist.d3;
       active_key = `d3`;
+      break;
+    // D#3
+    case `KeyE`:
+      note = notelist.d3sharp;
+      active_key = `d3sharp`;
       break;
     // E3
     case `KeyC`:
@@ -48,15 +57,30 @@ function noteOn (key, repeat) {
       note = notelist.f3;
       active_key = `f3`;
       break;
+    // F#3
+    case `KeyR`:
+      note = notelist.f3sharp;
+      active_key = `f3sharp`;
+      break;
     // G3
     case `KeyB`:
       note = notelist.g3;
       active_key = `g3`;
       break;
+    // G#3
+    case `KeyT`:
+      note = notelist.g3sharp;
+      active_key = `g3sharp`;
+      break;
     // A3
     case `KeyN`:
       note = notelist.a3;
       active_key = `a3`;
+      break;
+    // A#3
+    case `KeyY`:
+      note = notelist.a3sharp;
+      active_key = `a3sharp`;
       break;
     // B3
     case `KeyM`:
@@ -73,10 +97,20 @@ function noteOn (key, repeat) {
       note = notelist.c4;
       active_key = `c4`;
       break;
+    // C#4
+    case `KeyU`:
+      note = notelist.c4sharp;
+      active_key = `c4sharp`;
+      break;
     // D4
     case `KeyD`:
       note = notelist.d4;
       active_key = `d4`;
+      break;
+    // D#4
+    case `KeyI`:
+      note = notelist.d4sharp;
+      active_key = `d4sharp`;
       break;
     // E4
     case `KeyF`:
@@ -88,15 +122,30 @@ function noteOn (key, repeat) {
       note = notelist.f4;
       active_key = `f4`;
       break;
+    // C#4
+    case `KeyO`:
+      note = notelist.f4sharp;
+      active_key = `f4sharp`;
+      break;
     // G4
     case `KeyH`:
       note = notelist.g4;
       active_key = `g4`;
       break;
+    // G#4
+    case `KeyP`:
+      note = notelist.g4sharp;
+      active_key = `g4sharp`;
+      break;
     // A4
     case `KeyJ`:
       note = notelist.a4;
       active_key = `a4`;
+      break;
+    // A#4
+    case `BracketLeft`:
+      note = notelist.a4sharp;
+      active_key = `a4sharp`;
       break;
     // B4
     case `KeyK`:
@@ -110,6 +159,7 @@ function noteOn (key, repeat) {
       active_key = `c5`;
       break;
     default:
+      console.log(event.code)
       alert(`Key has not been mapped yet`);
 
   }
@@ -129,8 +179,9 @@ function noteOn (key, repeat) {
   }
 
   let timeoutsus = sustain * 1000;
+
   document.getElementById(active_key).style.backgroundColor = `grey`;
-  setTimeout(function () {document.getElementById(active_key).style.backgroundColor = `white`;}, timeoutsus);
+  setTimeout(function () {document.getElementById(active_key).style.backgroundColor = null;}, timeoutsus);
 
   const osci = audioCtx.createOscillator();
   osci.type = wave;
@@ -160,7 +211,7 @@ const canvas = document.getElementById(`canvas1`);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// To get acsess to 2D library
+// To get access to 2D library
 const ctx = canvas.getContext(`2d`);
 
 // ----------------------------------------------------------------------------------
@@ -168,30 +219,40 @@ const ctx = canvas.getContext(`2d`);
 const notelist = {
 
   c3:130.81,
+  c3sharp:138.59,
   d3:146.83,
+  d3sharp:155.56,
   e3:164.81,
   f3:174.61,
+  f3sharp:185.00,
   g3:196.00,
+  g3sharp:207.65,
   a3:220.00,
+  a3sharp:233.08,
   b3:246.94,
 
   c4:261.63,
+  c4sharp:277.18,
   d4:293.66,
+  d4sharp:311.13,
   e4:329.63,
   f4:349.23,
+  f4sharp:369.99,
   g4:392.00,
+  g4sharp:415.30,
   a4:440.00,
+  a4sharp:466.16,
   b4:493.88,
 
   c5:523.25
 };
-// Main audio compenent
+// Main audio competent
 const audioCtx = new AudioContext();
 
 // Variable to hold waveform type value
 let wave;
 wave = `square`;
-// Variable to holde sustin value
+// Variable to hold sustain value
 let sustain;
 
 const demo = new Audio(`demo.mp3`);
